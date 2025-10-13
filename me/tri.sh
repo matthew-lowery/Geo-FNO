d35626cfb25c1db6e8d0dbd66e76e998592b3212
matthew-lowery <mattlowery10@gmail.com>
2025-10-13 12:57:04 -0600

    cleanup slurm scr

--- me/tri.sh
+++ me/tri.sh
@@ -14,7 +14,7 @@ sp() {
 #SBATCH --partition=gpuA100x4,gpuA100x8
 #SBATCH --account=bfbk-delta-gpu
 #SBATCH --job-name=myjob
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
 #SBATCH --constraint="scratch"
 #SBATCH --gpus-per-node=1
 #SBATCH --output=./out/%x_%A.out
@@ -28,16 +28,14 @@ EOF
 }
 
 
-
+j
 for dataset in "flow_cylinder_shedding" "flow_cylinder_laminar" "taylor_green_numerical" "taylor_green_exact" "backward_facing_step" "buoyancy_cavity_flow" "lid_cavity_flow" "merge_vortices"; do
 for width in 16 32 64 128; do
-sp "python3 -u ramansh.py --dataset=$dataset --norm-grid --width=$width --depth=3 --res-1d=30"
-done
-for depth in 2 3 4 5; do
-sp "python3 -u ramansh.py --dataset=$dataset --norm-grid --width=32 --depth=$depth --res-1d=30"
+sp "python3 -u ramansh.py --dataset=$dataset --norm-grid --width=$width --res1d=30"
 done
 for s in 30 40 50 60; do
-sp "python3 -u ramansh.py --dataset=$dataset --modes=$((s/2)) --norm-grid --width=32 --depth=3 --res-1d=30"
+sp "python3 -u ramansh.py --dataset=$dataset --modes=$((s/2)) --norm-grid --width=32 --res1d=30"
 done
+sp "python3 -u ramansh.py --dataset=$dataset --width=16 --res1d=30"
 done
 
